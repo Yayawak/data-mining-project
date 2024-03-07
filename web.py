5f37ffeea4d352192800742c5df9e62af33940f4
Madara <elio.interesante@gmail.com>
2024-03-07 23:04:26 +0700

    using steramlint

--- web.py
+++ web.py
@@ -1,30 +1,7 @@
-from flask import Flask
+import streamlit as st 
 
-app = Flask(__name__)
+st.header("Hello World 👏")
+st.write("This is my first app")
 
-@app.route("/")
-def hello_world():
-    return f"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ url_for('static', filename='main.css') }}">
-    <script src="{{url_for('static', filename='index.js')}}"></script>
-</head>
-<body>
-    <div id="container">
-        <input type="file" id="csvFileInput" accept=".csv">
-        <button id="submitButton">Submit</button>
-        <div id="dataDisplay"></div>
-    </div>
-
-</body>
-</html>
-"""
-
-if __name__ == "__main__":
-    
-    app.run(port=7777, debug=True)
+dataset_name = "Breast cancel"
+csv_file = st.file_uploader(f"upload you {dataset_name} file.csv ", ['.csv'])
